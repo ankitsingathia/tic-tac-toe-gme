4899bb1ed430bf278ec4380969a69df1ec6dc449
first-demo <toankitsingathia@gmail.com>
2025-01-02 17:32:33 +0530

    updated the logic for edge cases

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let boxes = document.querySelectorAll(".box"); // Select all elements with the class "box"
 let resetBtn = document.querySelector("#reset-button"); // Select the reset button by its ID
-let newgamebtn = document.querySelector("#new-btn");
-let msgcontainer = document.querySelector(".msg");
-let msg = document.querySelector("#p");
+let newgamebtn = document.querySelector("#new-btn"); // New game button
+let msgcontainer = document.querySelector(".msg"); // Message container
+let msg = document.querySelector("#p"); // Message text
 
-// Track the player's turn
 let turn0 = true; // True for Player X, False for Player O
 
 const winpattern = [
@@ -18,49 +17,70 @@ const winpattern = [
     [6, 7, 8],
 ];
 
-// Add event listeners to each box
-boxes.forEach((box, index) => {
-    box.addEventListener("click", () => {
-        console.log(`Box ${index} clicked`);
-        if(turn0){
-            box.innerText = "O";
-            turn0=false;
-            
-        }
-        else {//playerx
-            box.innerText = "X"
-            turn0 = true;
-        }
-        box.disabled = true;
-        // Add game logic here
-        checkwinner();
+// Reset the game state
+const resetGame = () => {
+    turn0 = true;
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
     });
-});
+    msgcontainer.classList.add("hide");
+    msg.innerText = "";
+};
 
-// Add functionality to reset button
-resetBtn.addEventListener("click", () => {
-    console.log("Game reset");
-    // Reset the game logic here
-});
-const showwinner = (winner) =>{
-    pmsg.innerText= 'Congratulations, Winner is ${winner}';
-    msg.classList.remove("hide");
-}
+// Disable all boxes after the game ends
+const disableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+};
 
-const checkwinner = () => {
-    for (let pattern of winpattern){
-        // console.log(pattern[0],pattern[1],pattern[2] );
-        // console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText );
-        
+// Show the winner
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disableBoxes();
+};
+
+// Check if there is a winner
+const checkWinner = () => {
+    for (let pattern of winpattern) {
         let pos1val = boxes[pattern[0]].innerText;
         let pos2val = boxes[pattern[1]].innerText;
         let pos3val = boxes[pattern[2]].innerText;
-        //lets check any element is empty
-        if(pos1val !="" && pos2val != "" && pos3val != ""){
-            if(pos1val===pos2val && pos2val === pos3val){
-                console.log("Winner",pos1val);
-                showwinner(pos1val);
+
+        if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                showWinner(pos1val);
+                return;
             }
         }
     }
-}
+    // Check for a draw
+    let draw = Array.from(boxes).every((box) => box.innerText !== "");
+    if (draw) {
+        msg.innerText = "It's a Draw!";
+        msgcontainer.classList.remove("hide");
+        disableBoxes();
+    }
+};
+
+// Add click event listeners to each box
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn0) {
+            box.innerText = "O";
+        } else {
+            box.innerText = "X";
+        }
+        box.disabled = true;
+        turn0 = !turn0;
+        checkWinner();
+    });
+});
+
+// Add event listener to reset button
+resetBtn.addEventListener("click", resetGame);
+
+// Add event listener to new game button
+newgamebtn.addEventListener("click", resetGame);
